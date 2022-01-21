77cb42e78f09cde231905106ebdaa8bab67401eb
Roshan K menon <rkmenonhotdude@gmail.com>
2022-01-22 00:15:30 +0530

    password fix

--- util/database.js
+++ util/database.js
@@ -7,7 +7,7 @@ const DATABASE_URL = "ulsq0qqx999wqz84.chr7pe7iynqr.eu-west-1.rds.amazonaws.com"
 //   host:'localhost'  
 // });
 
-const sequelize = new Sequelize("whk5evemig1wljrb", "infs8oq2zsh1c5hi", "g05fs039g36aqnp9 ", {
+const sequelize = new Sequelize("whk5evemig1wljrb", "infs8oq2zsh1c5hi", "g05fs039g36aqnp9", {
   dialect: "mysql",
   port:3306,
   database:'whk5evemig1wljrb',  
