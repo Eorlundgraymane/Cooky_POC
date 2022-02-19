b287d7979fce27f6a5b3fff57ae7caefb1653bc0
Roshan K menon <rkmenonhotdude@gmail.com>
2022-02-19 15:35:05 +0530

    update

--- controllers/createController.js
+++ controllers/createController.js
@@ -20,42 +20,50 @@ exports.getIndex = (req,res,next) => {
 exports.postRecipe = (req,res,next) => {
     console.log("Home Controller Get Index");
     console.log(req);
-    Recipe.create({
-        title:req.body.recipeTitleText,
-        description:req.body.recipeDescription,        
-    })
-    .then((recipe) => {
-        req.body.ingredientList.split(',').forEach(ingredient => {
-            Ingredient.findOne({where : {title:ingredient}})
-            .then((ing) => {
-                RecipeIngredient.create({
-                    recipeId: recipe.id,
-                    ingredientId: ing.id
-                }).then((ingr) => {
-                    console.log("Recipe Inserted");
-                    Ingredient.findAll()
-                    .then((ingredients) => {
-                        res.render("create/index",{
-                            path: "create",
-                            pageTitle: "Cooky Creator",
-                            ingredients: ingredients
-                        });
+    if(/^\s*$/.test(req.body.recipeTitleText) || /^\s*$/.test(req.body.recipeDescription) || /^\s*$/.test(req.body.ingredientList)){
+        return res.status(500);
+    }
+    else{
+        Recipe.create({
+            title:req.body.recipeTitleText,
+            description:req.body.recipeDescription,        
+        })
+        .then((recipe) => {
+            req.body.ingredientList.split(',').forEach(ingredient => {
+                Ingredient.findOne({where : {title:ingredient}})
+                .then((ing) => {
+                    RecipeIngredient.create({
+                        recipeId: recipe.id,
+                        ingredientId: ing.id
+                    }).then((ingr) => {
+                        console.log("Recipe Inserted");
+                        Ingredient.findAll()
+                        .then((ingredients) => {
+                            res.render("create/index",{
+                                path: "create",
+                                pageTitle: "Cooky Creator",
+                                ingredients: ingredients
+                            });
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                            return res.status(500);
+                        });    
                     })
                     .catch((err) => {
                         console.log(err);
-                    });    
+                        return res.status(500);
+                    });
                 })
                 .catch((err) => {
                     console.log(err);
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-            }); 
-        });        
-    })
-    .catch((err) => {
-        console.log(err);
-    }); 
-
+                    return res.status(500);
+                }); 
+            });        
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500);
+        }); 
+    }    
 };
